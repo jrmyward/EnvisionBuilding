dcbf0789372130527f74ad3dbe55426f4a016e05
Jeremy Ward <jeremy.ward@digital-ocd.com>
2012-05-24 10:33:49 -0700

    Initial Project commit

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '/Users/jrmy/Sites/clients/envision_building_company/ebc_dm/lib/vendor/diem/dmCorePlugin/lib/core/dm.php';
+dm::start();
+
+class ProjectConfiguration extends dmProjectConfiguration
+{
+
+  public function setup()
+  {
+    parent::setup();
+    
+    $this->enablePlugins(array(
+      // add plugins you want to enable here
+    ));
+
+    $this->setWebDir(sfConfig::get('sf_root_dir').'/web');
+  }
+  
+}
---
+++ lib/form/doctrine/dmCorePlugin/DmLayoutForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * DmLayout form.
+ *
+ * @package    ebc_dm
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class DmLayoutForm extends PluginDmLayoutForm
+{
+  public function configure()
+  {
+  }
+}
